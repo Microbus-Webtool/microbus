128b9d79246318d602e74f275dbeed5fdb1cde37
Suman12378 <rajneeshthakur12378@gmail.com>
2023-10-24 02:15:31 +0530

    navbar-mid-page

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <Router>
+
         <Navbar/>
+
         <Routes>
           <Route path='/' element={<Home/>} />
         </Routes>
+
         <Footer/>
+        
     </Router>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Logo from '../asserts/micro_logo_black.png'
+import "./styles/navbar.scss"
 
 
 const Navbar = () => {
+
+ 
   return (
-    
-  <div>
-    Navbar
-  </div>
+  
+    <nav className="navbar">
+     <div className="logo"><img src={Logo} alt="" /></div>
+
+      <div className="menu">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Events</a></li>
+          <li><a href="#">Team</a></li>
+        </ul>
+      </div>
+      
+    </nav>
+
+
    
   )
 }
