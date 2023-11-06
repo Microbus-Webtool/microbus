f6b3539a5272d89629ec03fb0643c3a9f8ff9527
Himanshu-swn <swn.himanshu@gmail.com>
2023-11-06 19:01:49 +0530

    teampage updated

--- src/components/TeamPage.jsx
+++ src/components/TeamPage.jsx
@@ -1,57 +1,101 @@
 import React from "react";
 import "../styles/TeamPage.scss";
-import TinySlider from "../components/tinySlider";
-import Carousel from "./Carousel";
+import Card from "./Card";
 
-
-const images = [
-  "/assets/slider/image2.png",
-  "/assets/slider/image2.png",
-  "/assets/slider/image2.png",
-  "/assets/slider/image2.png",
-  "/assets/slider/image2.png",
-  "/assets/slider/image2.png",
-  "/assets/slider/image2.png",
-  "/assets/slider/image2.png",
-  "/assets/slider/image2.png",
-  "/assets/slider/image2.png",
-  "/assets/slider/image2.png",
-  "/assets/slider/image2.png",
-  "/assets/slider/image2.png",
-  "/assets/slider/image2.png",
-  "/assets/slider/image2.png",
-  "/assets/slider/image2.png",
-  "/assets/slider/image2.png",
-  "/assets/slider/image2.png",
+const teamMembers = [
+  {
+    image: "/assets/slider/image2.png",
+    name: "John Doe",
+    designation: "Developer",
+  },
+  {
+    image: "/assets/slider/image2.png",
+    name: "Jane Doe",
+    designation: "Designer",
+  },
+  {
+    image: "/assets/slider/image2.png",
+    name: "Jane Doe",
+    designation: "Designer",
+  },
+  {
+    image: "/assets/slider/image2.png",
+    name: "Jane Doe",
+    designation: "Designer",
+  },
+  {
+    image: "/assets/slider/image2.png",
+    name: "Jane Doe",
+    designation: "Designer",
+  },
+  {
+    image: "/assets/slider/image2.png",
+    name: "Jane Doe",
+    designation: "Designer",
+  },
+  {
+    image: "/assets/slider/image2.png",
+    name: "Jane Doe",
+    designation: "Designer",
+  },
+  {
+    image: "/assets/slider/image2.png",
+    name: "Jane Doe",
+    designation: "Designer",
+  },
+  // Add more team members as needed
 ];
 
 const TeamPage = () => {
-
-
-    return (
-      <div>
-        <h1>TEAMS</h1>
-
-        <div className="slider-container-1">
-          <h3>DEVELOPERS</h3>
-          <Carousel images={images}/>
+  return (
+    <div>
+      <h1>TEAMS</h1>
+      <div className="team-container">
+        <h3 className="heading1">Developers</h3>
+        <div className="allCards">
+          {teamMembers.map((member, index) => (
+            <Card
+              key={index}
+              image={member.image}
+              name={member.name}
+              designation={member.designation}
+            />
+          ))}
         </div>
-        
-        <br />
-        <div className="slider-container-2">
-          <h3>DESIGNERS</h3>
-          <Carousel images={images}/>
-        </div>
-
-        <br />
-        <div className="slider-container-3">
-          <h3>CONTENT WRITERS</h3>
-          <Carousel images={images}/>
-        </div>
-        <br />
-        <br />
       </div>
-    );
+      <br />
+      <br />
+      <div className="team-container">
+        <h3 className="heading1">Designers</h3>
+        <div className="allCards">
+          {teamMembers.map((member, index) => (
+            <Card
+              key={index}
+              image={member.image}
+              name={member.name}
+              designation={member.designation}
+            />
+          ))}
+        </div>
+      </div>
+      <br />
+      <br />
+      <div className="team-container">
+        <h3 className="heading1">Content Writers</h3>
+        <div className="allCards">
+          {teamMembers.map((member, index) => (
+            <Card
+              key={index}
+              image={member.image}
+              name={member.name}
+              designation={member.designation}
+            />
+          ))}
+        </div>
+      </div>
+      <br /> <br /> <br /> <br />
+    </div>
+  );
 };
 
 export default TeamPage;
