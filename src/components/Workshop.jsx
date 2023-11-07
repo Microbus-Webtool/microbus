5f734d00369f80b588b92f32f12eb54afff66427
akshat209141 <113182777+akshat209141@users.noreply.github.com>
2023-11-07 22:44:15 +0530

    Create Workshop.jsx

---
+++ src/components/Workshop.jsx
@@ -0,0 +1,41 @@
+import React from "react"
+import '../styles/Workshop.scss'
+const Workshop = () => 
+{
+    return (
+        <div className="workshop-container">
+            <div className="heading">
+                <h6> Workshop </h6>
+            </div>
+
+            <div className="card-container">
+                <div className="card-heading">
+                    DESIGN
+                </div>
+                <div className="card-content">
+                    <div className="Upper-rectangle">
+                        <div className="Lower-rectangle">
+                            Get big, quick. Solana is made to handle thousands of transactions per second, and fees for both developers and users remain less than $0.01.Get big, quick. Solana is made to handle thousands of transactions per second, and fees for both developers and users remain less than $0.01.Get big, quick. Solana is made to handle thousands of transactionS.
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div className="card-container">
+                <div className="card-heading">
+                    IOT
+                </div>
+                <div className="card-content">
+                    <div className="Upper-rectangle">
+                        <div className="Lower-rectangle">
+                            Get big, quick. Solana is made to handle thousands of transactions per second, and fees for both developers and users remain less than $0.01.Get big, quick. Solana is made to handle thousands of transactions per second, and fees for both developers and users remain less than $0.01.Get big, quick. Solana is made to handle thousands of transactionS.
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+export default Workshop;
