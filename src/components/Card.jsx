137c680568c4b138dd8457db3bc2f37c0556a114
Himanshu-swn <swn.himanshu@gmail.com>
2023-11-19 01:48:20 +0530

    made teampage responsive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,19 @@ const Card = ({ image, name, designation }) => {
       <img src={image} alt={name} className="card-image" />
       <div className="card-details">
         <h2 className="card-title">{name}</h2>
-        <p className="card-designation">{designation}</p>
+        <a href="https://www.instagram.com/swn.himanshu/">
+          <img
+            className="socials"
+            src="/assets/instagram.svg"
+            alt="instagram"
+          />
+        </a>
+        <a href="https://www.instagram.com/swn.himanshu/">
+          <img className="socials" src="/assets/linkedin.svg" alt="linkedin" />
+        </a>
+        <a href="https://www.instagram.com/swn.himanshu/">
+          <img className="socials" src="/assets/github.svg" alt="github" />
+        </a>
       </div>
     </div>
   );
